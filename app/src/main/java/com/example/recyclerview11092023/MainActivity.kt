d77809b4adfd533fbdb1b9dfe9346031746382fe
phamtanphat <phatdroid94@gmail.com>
2023-11-25 21:13:30 +0700

    set adapter

--- app/src/main/java/com/example/recyclerview11092023/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview11092023/MainActivity.kt
@@ -11,13 +11,24 @@ import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import java.text.SimpleDateFormat
 import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var bookAdapter: BookAdapter
+    private val listBook: List<Book> by lazy {
+        Book.getListBooks()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recyclerView = findViewById(R.id.recyclerview_book)
+        bookAdapter = BookAdapter()
+        bookAdapter.setListBook(listBook)
+        recyclerView.adapter = bookAdapter
     }
 }
