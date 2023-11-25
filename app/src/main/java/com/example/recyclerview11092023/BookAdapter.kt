7ea1c02eacfe2f7f1e7d6afb241871128d314d51
phamtanphat <phatdroid94@gmail.com>
2023-11-25 19:57:13 +0700

    setup adapter

--- app/src/main/java/com/example/recyclerview11092023/BookAdapter.kt
+++ app/src/main/java/com/example/recyclerview11092023/BookAdapter.kt
@@ -1,14 +1,21 @@
 package com.example.recyclerview11092023
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class BookAdapter: RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
+class BookAdapter : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
+    private var listBook: List<Book> = emptyList()
 
-    class BookViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    fun setListBook(listBook: List<Book>) {
+        this.listBook = listBook
+    }
+
+    class BookViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private var image: ImageView = view.findViewById(R.id.image_view_book)
         private var tvName: TextView = view.findViewById(R.id.text_view_book_name)
         private var tvPostDateAndViewCount: TextView = view.findViewById(R.id.text_view_book_post_date_and_view_count)
@@ -16,11 +23,13 @@ class BookAdapter: RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.layout_item_book, parent, false)
+        return BookViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listBook.size
     }
 
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
